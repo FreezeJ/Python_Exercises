3fb1b443422be384b438139e36ebd2080fd83bb2
FreezeJ <120677982@qq.com>
2017-11-19 14:56:59 +0800

    巧妙使用a, b = b, c这种赋值方式

---
+++ 6.py
@@ -0,0 +1,17 @@
+#!/bin/usr/env python
+# coding=utf-8
+
+'''
+题目：斐波那契数列。
+'''
+
+def Fib(n):
+	a, b = 0, 1
+	if n == 1:return 0
+	if n == 2:return 1
+	for i in range(n-1):
+		c = a + b
+		a, b = b, c
+	return c
+
+print Fib(10)
