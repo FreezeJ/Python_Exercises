fea02a4ebafb0146a23758540c4c9b665bb7f6ce
FreezeJ <120677982@qq.com>
2017-11-19 15:06:25 +0800

    列表深copy、浅copy、倒序

---
+++ 7.py
@@ -0,0 +1,16 @@
+#!/bin/usr/env python
+# coding=utf-8
+
+'''
+题目：将一个列表的数据复制到另一个列表中。
+'''
+
+a = [1, 2, 3]
+print id(a)
+b = a[:]  # 浅copy
+print id(b)
+c = a  # 深copy
+print id(c)
+d = a[::-1] # 倒序
+print b
+print c
